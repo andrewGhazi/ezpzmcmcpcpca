b16ad964f1ac6698b371b346554eda3040897bca
Andrew Ghazi <andghazi@gmail.com>
2023-08-11 10:40:43 -0400

    alternate method to get W_id by dotting to chain 1

--- R/mcmc.R
+++ R/mcmc.R
@@ -53,5 +53,49 @@ pcpca_mcmc = function(X, Y, gamma, d = 1, verbose = TRUE,
 
   pcpca_fit = id_model2$sample(data = data_list,
                                init = init, ...)
+
   pcpca_fit
+  # stop("Ghazi needs to finish get_W_id()!"); pcpca_fit |> get_W_id(p = ncol(Y), k = d)
+}
+
+# These two functions dot the draws of W onto the posterior mean of the first
+# chain (instead of W_mle like the sampler) to get W_id. It doesn't really make
+# a difference.
+dot_to_11 = function(draw, draw11, p, k) {
+
+  # For each latent dimension in the draw, check if it points in roughly the
+  # same direction as draw #1, according to the sign of the dot product. If not,
+  # multiply it by -1.
+
+  dot_prods = draw %*% d11w
+
+  for (i in 1:k) {
+    if (dot_prods[1,i] < 0) {
+      i_cols = (1:p)+ (i-1)*p
+      draw[i_cols] = -1*draw[i_cols]
+    }
+  }
+
+  return(draw)
+}
+
+get_W_id = function(pcpca_fit, p, k) {
+  draw_mat = pcpca_fit$draws("W", format = 'matrix')
+
+  draw11 = pcpca_fit$draws('W')[,1,] |>
+    posterior::summarise_draws(mean = mean) |>
+    getElement("mean")
+
+  d11w = matrix(nrow = p*k, ncol = k, 0)
+  for (i in 1:k){
+    d11w[(1:p) + (i-1)*p,i] = draw11[(1:p) + (i-1)*p]
+  }
+
+  W_id = draw_mat |> apply(1, dot_to_11, d11w, p, k) |> t()
+
+  colnames(W_id) = colnames(W_id) |>
+    gsub("W", "W_id", x = _)
+
+  return(W_id)
+
 }
